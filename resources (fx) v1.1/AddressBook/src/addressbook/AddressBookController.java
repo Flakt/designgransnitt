9d049c74fcc42ff1cd6aee6d98d194665ce70264
Illnoid <stefan2846@hotmail.com>
2018-03-20 20:46:00 +0100

    Preliminary version of lab1 done, can't however switch between contacts

--- resources (fx) v1.1/AddressBook/src/addressbook/AddressBookController.java
+++ resources (fx) v1.1/AddressBook/src/addressbook/AddressBookController.java
@@ -4,6 +4,9 @@ package addressbook;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,22 +24,35 @@ public class AddressBookController implements Initializable {
     @FXML private Button newButton;
     @FXML private Button deleteButton;
     @FXML private SplitPane splitPane;
-    @FXML private ListView listView;
+    @FXML private ListView listView;  //    Ska enlig labb vara contactsList
 
-    @FXML private TextField firstNameField;
-    @FXML private TextField lastNameField;
-    @FXML private TextField phoneField;
-    @FXML private TextField mailField;
-    @FXML private TextField addressField;
-    @FXML private TextField cityField;
-    @FXML private TextField postCodeField;
+    @FXML private TextField firstNameTextField;
+    @FXML private TextField lastNameTextField;
+    @FXML private TextField phoneTextField;
+    @FXML private TextField mailTextField;
+    @FXML private TextField addressTextField;
+    @FXML private TextField cityTextField;
+    @FXML private TextField postCodeTextField;
 
     private Presenter presenter;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        presenter = new Presenter(listView,firstNameField,lastNameField,phoneField,mailField,addressField,cityField,postCodeField);
+        presenter = new Presenter(listView,firstNameTextField,lastNameTextField,phoneTextField,mailTextField,addressTextField,cityTextField,postCodeTextField);
         presenter.init();
+        listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+
+            }
+        });
+        firstNameTextField.focusedProperty().addListener(new TextFieldListener(firstNameTextField));
+        lastNameTextField.focusedProperty().addListener(new TextFieldListener(lastNameTextField));
+        phoneTextField.focusedProperty().addListener(new TextFieldListener(phoneTextField));
+        mailTextField.focusedProperty().addListener(new TextFieldListener(mailTextField));
+        addressTextField.focusedProperty().addListener(new TextFieldListener(addressTextField));
+        cityTextField.focusedProperty().addListener(new TextFieldListener(cityTextField));
+        postCodeTextField.focusedProperty().addListener(new TextFieldListener(postCodeTextField));
     }
     
     @FXML 
@@ -62,5 +78,33 @@ public class AddressBookController implements Initializable {
     @FXML
     protected void newButtonActionPerformed (ActionEvent event) {
         presenter.newContact();
+    }
+
+    @FXML
+    protected void deleteButtonActionPerformed (ActionEvent event) {
+        presenter.removeCurrentContact();
+    }
+
+    @FXML
+    protected void textFieldActionPerformed(ActionEvent event) {
+        presenter.textFieldActionPerformed(event);
+    }
+
+    private class TextFieldListener implements ChangeListener<Boolean> {
+        private TextField textField;
+
+        public TextFieldListener(TextField textField) {
+            this.textField = textField;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if (newValue) {
+                presenter.textFieldFocusGained(textField);
+            }
+            else {
+                presenter.textFieldFocusLost(textField);
+            }
+        }
     }
 }
